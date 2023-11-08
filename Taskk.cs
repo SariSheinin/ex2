12abf4cb6e1793fa2c1473d8a64b504b1de114e2
326339876 <37326339876@mby.co.il>
2023-11-08 16:55:59 +0200

    malki delete the cmments

--- Taskk.cs
+++ Taskk.cs
@@ -2,14 +2,13 @@
 {
     public enum Preference
     {
-       a,b,c,d,e
+        A,B,C,D,E
     }
     public class Taskk
     {
         public string Title { get; set; }
         public string Description { get; set; }
-        public DateTime TargetDate { get; set; }//////dsasdf
+        public DateTime TargetDate { get; set; }
         public Preference MyPreference { get; set; }
-///ggf
     }
 }
